295000c1ce6a9c0aba3a52a357f7e1bfee3dd624
underscorediscovery <sven@underscorediscovery.com>
2015-01-31 07:57:29 -0330

    Fix ArrayBuffer constructor being the right naming

--- std/haxe/io/buffer/ArrayBuffer.hx
+++ std/haxe/io/buffer/ArrayBuffer.hx
@@ -11,8 +11,8 @@ import haxe.io.Bytes;
 
 @:forward()
 abstract ArrayBuffer(Bytes) from Bytes to Bytes {
-    public inline function new( nElements:Int ) {
-        this = Bytes.alloc( nElements );
+    public inline function new( byteLength:Int ) {
+        this = Bytes.alloc( byteLength );
     }
 }
 
