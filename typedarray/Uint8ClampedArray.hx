6f575a9d3c7598be4ac45eff99cec1c37a260541
underscorediscovery <sven@underscorediscovery.com>
2015-03-23 02:31:57 -0230

    minor fixes for haxe 3.2

--- typedarray/Uint8ClampedArray.hx
+++ typedarray/Uint8ClampedArray.hx
@@ -39,7 +39,11 @@ package typedarray;
         }
 
         public function toBytes() : haxe.io.Bytes {
+            #if (haxe_ver < 3.2)
             return @:privateAccess new haxe.io.Bytes( this.byteLength, cast new js.html.Uint8Array(this.buffer) );
+            #else
+                return @:privateAccess new haxe.io.Bytes( cast new js.html.Uint8Array(this.buffer) );
+            #end
         }
 
         //internal
