6862a9f564e8db10e72e31defde3453c3b1dbe4a
underscorediscovery <sven@underscorediscovery.com>
2015-09-19 16:30:54 -0230

    remove superfluous return from array access

--- typedarray/Uint8ClampedArray.hx
+++ typedarray/Uint8ClampedArray.hx
@@ -33,7 +33,7 @@ package typedarray;
             }
         }
 
-        @:arrayAccess @:extern inline function __set(idx:Int, val:UInt) return this[idx] = _clamp(val);
+        @:arrayAccess @:extern inline function __set(idx:Int, val:UInt) : Void this[idx] = _clamp(val);
         @:arrayAccess @:extern inline function __get(idx:Int) : UInt return this[idx];
 
 
@@ -125,9 +125,8 @@ package typedarray;
 
         @:noCompletion
         @:arrayAccess @:extern
-        public inline function __set(idx:Int, val:UInt) {
+        public inline function __set(idx:Int, val:UInt) : Void {
             ArrayBufferIO.setUint8Clamped(this.buffer, this.byteOffset+idx, val);
-            return val;
         }
 
         inline function toString() return 'Uint8ClampedArray [byteLength:${this.byteLength}, length:${this.length}]';
